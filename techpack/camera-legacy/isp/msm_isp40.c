064e93b9e2f9ec1adbde963a2ceda5771718c512
Mirah Syakilla <mirahsyakilla@gmail.com>
2026-07-20 07:11:16 +0800

    media: restore split VFE40 EPOCH1
    Validate and clamp the CAMIF epoch_line1 value before programming it. Use the active vfe_dev split flag so only split VFE40 paths receive the EPOCH1 register write, while preserving the existing non-split/front-camera path. Keep the normal CAMIF state update from overwriting the split value.

--- techpack/camera-legacy/isp/msm_isp40.c
+++ techpack/camera-legacy/isp/msm_isp40.c
@@ -1344,6 +1344,7 @@ static void msm_vfe40_cfg_camif(struct vfe_device *vfe_dev,
 	struct msm_vfe_pix_cfg *pix_cfg)
 {
 	uint16_t first_pixel, last_pixel, first_line, last_line;
+	uint16_t epoch_line1;
 	struct msm_vfe_camif_cfg *camif_cfg = &pix_cfg->camif_cfg;
 	uint32_t val, subsample_period, subsample_pattern;
 	struct msm_vfe_camif_subsample_cfg *subsample_cfg =
@@ -1359,6 +1360,14 @@ static void msm_vfe40_cfg_camif(struct vfe_device *vfe_dev,
 	last_pixel = camif_cfg->last_pixel;
 	first_line = camif_cfg->first_line;
 	last_line = camif_cfg->last_line;
+	epoch_line1 = camif_cfg->epoch_line1;
+
+	if (!epoch_line1 || epoch_line1 > last_line)
+		epoch_line1 = last_line > 50 ? last_line - 50 : last_line;
+
+	if (last_line - epoch_line1 > 100)
+		epoch_line1 = last_line - 100;
+
 	subsample_period = camif_cfg->subsample_cfg.irq_subsample_period;
 	subsample_pattern = camif_cfg->subsample_cfg.irq_subsample_pattern;
 
@@ -1370,6 +1379,14 @@ static void msm_vfe40_cfg_camif(struct vfe_device *vfe_dev,
 
 	legacy_msm_camera_io_w(first_line << 16 | last_line,
 	vfe_dev->vfe_base + 0x308);
+
+	if (vfe_dev->is_split) {
+		legacy_msm_camera_io_w_mb(0x140000 | epoch_line1,
+			vfe_dev->vfe_base + 0x318);
+		pr_info("%s: vfe %d split CAMIF epoch1=%u last_line=%u\n",
+			__func__, vfe_dev->pdev->id, epoch_line1, last_line);
+	}
+
 	if (subsample_period && subsample_pattern) {
 		val = legacy_msm_camera_io_r(vfe_dev->vfe_base + 0x2F8);
 		val &= 0xFFE0FFFF;
@@ -1493,7 +1510,9 @@ static void msm_vfe40_update_camif_state(struct vfe_device *vfe_dev,
 		legacy_msm_camera_io_w_mb(0x1, vfe_dev->vfe_base + 0x24);
 		msm_vfe40_config_irq(vfe_dev, 0x17, 0x81,
 				MSM_ISP_IRQ_ENABLE);
-		legacy_msm_camera_io_w_mb(0x140000, vfe_dev->vfe_base + 0x318);
+		if (!vfe_dev->is_split)
+			legacy_msm_camera_io_w_mb(0x140000,
+				vfe_dev->vfe_base + 0x318);
 
 		bus_en =
 			((vfe_dev->axi_data.
@@ -2364,4 +2383,3 @@ module_init(msm_vfe40_init_module);
 module_exit(msm_vfe40_exit_module);
 MODULE_DESCRIPTION("MSM VFE40 driver");
 MODULE_LICENSE("GPL v2");
-
